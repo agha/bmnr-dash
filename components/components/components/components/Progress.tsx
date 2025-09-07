75e4e111965513b38b61ed2ea20d2e65f7a29393
agha <agha@dinartrade.com>
2025-09-06 14:33:24 -1000

    Delete components/components directory

--- components/components/components/components/Progress.tsx
+++
@@ -1,6 +0,0 @@
-export default function Progress({value}:{value:number}) {
-  const v = Math.max(0, Math.min(100, value));
-  return (
-    <div className="progress"><div style={{width:`${v}%`}}/></div>
-  );
-}
